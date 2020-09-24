9d7fc4e11610db236ff4d5854cf7006b30194eef
venkey <venkeyvenkatesh885@gmail.com>
2020-09-24 14:31:38 +0530

    [venkey] Refactor. change in message

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -6,7 +6,7 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello capgemini!");
+            Console.WriteLine("Hello Bangalore!");
         }
     }
 }
